d10364e7dde35d3f3846760e10c18956c6635c2b
Zelgath <sebastian.mataczynski@gmail.com>
2020-02-19 11:22:32 +0100

    Header component is now visible on employees list view

--- task-reporting/src/app/core-module/core.module.ts
+++ task-reporting/src/app/core-module/core.module.ts
@@ -6,6 +6,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [SidebarComponent],
+  exports: [SidebarComponent],
   imports: [
     CommonModule
   ]
