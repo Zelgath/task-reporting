00440a3fad3b72f65c09dcc7200e3849b5e3b240
Zelgath <sebastian.mataczynski@gmail.com>
2020-03-03 22:20:41 +0100

    >Add employee form almost complete, added datepicker

--- task-reporting/src/app/employees/add-employee/add-employee.component.ts
+++ task-reporting/src/app/employees/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'tr-add-employee',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEmployeeComponent implements OnInit {
 
-  constructor() { }
+  employeeForm : FormGroup;
+
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit() {
+    this.employeeForm = this.buildEmployeeForm();
+  }
+
+  buildEmployeeForm() {
+    return this.formBuilder.group ({
+      firstName : '',
+      lastName : '',
+      email : '',
+      phoneNumber : '',
+      hireDate : '',
+      jobId : '',
+      salary : '',
+      managerId : '',
+      departmentId : ''
+    });
   }
 
 }
--- task-reporting/src/app/employees/employees.module.ts
+++ task-reporting/src/app/employees/employees.module.ts
@@ -10,7 +10,7 @@ import { SharedModule } from '../shared-module/shared.module';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { RouterModule } from '@angular/router';
 import { EmployeeResolve } from './employee-resolve.service';
-import { MatSortModule } from '@angular/material';
+import { MatSortModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule } from '@angular/material';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 
@@ -24,7 +24,11 @@ import { AddEmployeeComponent } from './add-employee/add-employee.component';
     SharedModule,
     RouterModule,
     MatSortModule,
-    FormsModule
+    FormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule
   ],
   providers: [EmployeesService, EmployeeResolve]
 })
